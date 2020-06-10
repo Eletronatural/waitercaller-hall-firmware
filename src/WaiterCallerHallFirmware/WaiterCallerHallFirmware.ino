2be0e94c4ad1330016b7d100456db2c7991e84ae
Gustavo Rubin <gusrubin@gmail.com>
2020-06-10 17:44:09 -0300

    LED pin 0 turns on when receive 1 on hall topic

--- src/WaiterCallerHallFirmware/WaiterCallerHallFirmware.ino
+++ src/WaiterCallerHallFirmware/WaiterCallerHallFirmware.ino
@@ -1,5 +1,5 @@
 /*
- * Waiter Caller Firmeware
+ * Waiter Caller Hall Device Firmeware
  * Validated for devices: ESP8266 ESP-01 
  * Author: Gustavo Rubin (gusrubin@gmail.com)
 */
@@ -23,12 +23,7 @@ const char *device_serial_number_file = "/device-serial-number.cfg";
 char deviceSerialNumber[30] = "";
 
 // constants won't change. They're used here to set pin numbers:
-const int buttonPin = 2; // the number of the pushbutton pin
 const int ledPin =  0; // the number of the LED pin
-
-// variables will change:
-int buttonState = 0; // variable for reading the pushbutton status
-bool buttonWasPressed = false;
 
 // Loads serial number from a file
 void loadDeviceSerialNumber() {
@@ -50,7 +45,6 @@ void loadDeviceSerialNumber() {
 }
 
 void setup_wifi() {
-
   delay(10);
   // We start by connecting to a WiFi network
   Serial.println();
@@ -84,11 +78,13 @@ void callback(char* topic, byte* payload, unsigned int length) {
 
   // Switch on the LED if an 1 was received as first character
   if ((char)payload[0] == '1') {
-    digitalWrite(BUILTIN_LED, LOW);   // Turn the LED on (Note that LOW is the voltage level
+    digitalWrite(BUILTIN_LED, LOW); // Turn the LED on (Note that LOW is the voltage level
+    digitalWrite(ledPin, LOW);
     // but actually the LED is on; this is because
     // it is active low on the ESP-01)
   } else {
-    digitalWrite(BUILTIN_LED, HIGH);  // Turn the LED off by making the voltage HIGH
+    digitalWrite(BUILTIN_LED, HIGH); // Turn the LED off by making the voltage HIGH
+    digitalWrite(ledPin, HIGH);
   }
 }
 
@@ -105,7 +101,7 @@ void reconnect() {
       // Once connected, publish an announcement...
       client.publish("waitercaller/device-start-notice", deviceSerialNumber);
       // ... and resubscribe
-      // client.subscribe("inTopic");
+      client.subscribe("waitercaller/hall");
     } else {
       Serial.print("failed, rc=");
       Serial.print(client.state());
@@ -120,14 +116,12 @@ void setup() {
   Serial.begin(115200);
   setup_wifi();
   Serial.println("Booting...");
- 
-  pinMode(BUILTIN_LED, OUTPUT); // Initialize the BUILTIN_LED pin as an output
+
   // initialize the LED pin as an output:
+  pinMode(BUILTIN_LED, OUTPUT); // Initialize the BUILTIN_LED pin as an output
+  digitalWrite(BUILTIN_LED, HIGH);
   pinMode(ledPin, OUTPUT);
   digitalWrite(ledPin, HIGH);
-  // initialize the pushbutton pin as an input:
-  pinMode(buttonPin, INPUT);
-  buttonWasPressed = false;
 
   if (!SPIFFS.begin()) {
     Serial.println("Failed to mount file system");
@@ -145,22 +139,4 @@ void loop() {
     reconnect();
   }
   client.loop();
-
-  // read the state of the pushbutton value:
-  buttonState = digitalRead(buttonPin);
-
-  // check if the pushbutton is pressed. If it is, the buttonState is LOW:
-  if (buttonState == LOW && buttonWasPressed == false) {
-    buttonWasPressed = true;
-    // turn LED on:
-    digitalWrite(ledPin, LOW);
-    client.publish("waitercaller/desk/1", "1");
-  }
-  if (buttonState == HIGH && buttonWasPressed == true) {
-    buttonWasPressed = false;
-    // turn LED off:
-    digitalWrite(ledPin, HIGH);
-    client.publish("waitercaller/desk/1", "0");
-  }
-
 }
